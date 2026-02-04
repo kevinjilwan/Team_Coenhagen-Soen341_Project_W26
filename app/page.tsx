bffecd9cb09aa57fece079d08fd6aa955464c5bb
JordanoPreteroti <jordanopreteroti@gmail.com>
2026-02-03 20:44:16 -0500

    Switch layout from padding to grid

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,15 @@ const font = Reenie_Beanie({ subsets: ["latin"], weight: "400" });
 
 function Logo() {
   return(
-  <Link href="/" className="top-[-25px] left-[150px] absolute">
-  <Image src="/logo.png" alt="Meal Major Logo" width={200} height={200} />
+  <Link href="/">
+    <Image src="/logo.png" alt="Meal Major Logo" width={200} height={200} />
   </Link>
   );
 }
 
 function HeaderText(){
   return (
-    <Link href="/" className={`top-[75px] left-[48%] -translate-x-1/2 -translate-y-1/2 absolute
-    text-8xl font-semibold text-center italic ${font.className}
-    text-gray-700`} >
+    <Link href="/" className={`text-8xl font-semibold text-center italic ${font.className} text-gray-700`} >
       Meal Major
     </Link>
   );
@@ -25,61 +23,69 @@ function HeaderText(){
 
 function WelcomeText(){
   return (
-    <p className={`top-[250px] left-[48%] -translate-x-1/2 -translate-y-1/2 absolute 
-    text-6xl text-gray-700 ${font.className}`}>
+    <p className={`text-6xl text-gray-700 ${font.className}`}>
       Welcome to Meal Major
-      </p>
+    </p>
     );
   }
 
 function SubWelcomeText(){
   return (
-    <p className={`top-[375px] left-[48%] -translate-x-1/2 -translate-y-1/2 absolute
-    text-2xl text-gray-700 text-center ${font.className}`} >
-MealMajor helps students plan meals and stay organized by managing recipes and creating a personalized 
-weekly meal plan. Easily track dietary preferences, assign meals to each day, and avoid repetition.
-    </p>
+  <p className={`text-2xl text-gray-700 ${font.className}`} >
+    MealMajor helps students plan meals and stay organized by managing recipes and creating a personalized <br />
+    weekly meal plan. Easily track dietary preferences, assign meals to each day, and avoid repetition.
+  </p>
   );
 }
 
 function GetStartedButton() {
   return (
-  <Link href="/auth/sign-up" className="top-[50px] right-[200px] absolute 
-  text-sm font-medium px-4 py-1.5 rounded-full
-  text-white bg-[#e0d5c5]">
-
-  Get started
-</Link>
+  <Link href="/auth/sign-up" className=" text-sm font-medium px-4 py-1.5 rounded-full
+    text-white bg-[#e0d5c5]">
+    Get started
+  </Link>
   );
 }
 
 function LoginButton() {
   return (
-  <Link href="/auth/login" className="top-[55%] left-[48%] -translate-x-1/2 -translate-y-1/2 absolute 
-  text-sm font-medium px-4 py-1.5 rounded-full
-  text-white bg-[#e0d5c5]">
-  Login
-</Link>
+  <Link href="/auth/login" className="text-sm font-medium px-4 py-1.5 rounded-full
+    text-white bg-[#e0d5c5]">
+    Login
+  </Link>
   );
 }
 
 export default function HomePage() {
   return (
-    <main className="h-screen bg-gradient-to-b from-stone-200 to-amber-100">
-    <header className="px-[150px] pt-[100px] flex items-center justify-between">
+    <main className="min-h-screen bg-gradient-to-b from-stone-200 to-amber-100 flex flex-col">
+      
+      <header className="grid grid-cols-3 items-center px-6 py-0 border-b border-stone-300">
+        <div className="justify-self-start px-40">
+          <Logo />
+        </div>
 
-<Logo />
-<HeaderText />
-<WelcomeText />
-<SubWelcomeText />
+        <div className="justify-self-center text-center">
+          <HeaderText />
+        </div>
 
-<GetStartedButton />
-<LoginButton />
+        <div className="justify-self-end px-40">
+          <GetStartedButton />
+        </div>
+
+        <hr className="border-stone-300" />
 
       </header>
-      <hr className="mt-12 border-stone-300" />
+
+      <section className="grid place-items-center text-center gap-10 mt-12">
+        <WelcomeText />
+        <SubWelcomeText />
+        <LoginButton />
+      </section>
+
     </main>
   );
 }
+
 
 
